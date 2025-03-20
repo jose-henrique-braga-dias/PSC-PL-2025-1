9a096b17c663f5a036aaa6541656589688e9b409
jose-henrique-braga-dias <josehbd10@gmail.com>
2025-03-20 11:33:31 -0300

    Criação de matrizes

---
+++ Matrizes/matriz2.java
@@ -0,0 +1,12 @@
+package Matrizes;
+
+public class matriz2 {
+    public static void main(String[] args) {
+        int[] valores = {1, 24, 46, 73, 81};
+        int contador;
+        for(contador = 0; contador < valores.length; contador++) {
+            System.out.println("Valor " + valores[contador]);
+
+        }
+    }
+}
---
+++ Matrizes/matriz3.java
@@ -0,0 +1,16 @@
+package Matrizes;
+
+public class matriz3 {
+    public static void main(String[] args) {
+        String[] nomes = { "José", "Carlos", "bernado"};
+        int[] numeros = {2, 5, 8, 10, 24};
+        for(String nome: nomes ) {
+            System.out.println("Nome " + nome);
+        }
+        for(int numero: numeros ) {
+            System.out.println("Numero " + numero);
+        }
+
+    }
+    
+}
